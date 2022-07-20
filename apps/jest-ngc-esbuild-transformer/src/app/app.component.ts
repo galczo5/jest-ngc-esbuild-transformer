62d6fa5981eb5309d34695564940366522e502d4
galczo5 <kamil.galek@gmail.com>
2022-07-20 21:36:20 +0200

    initial commit

---
+++ apps/jest-ngc-esbuild-transformer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jest-ngc-esbuild-transformer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'jest-ngc-esbuild-transformer';
+}
