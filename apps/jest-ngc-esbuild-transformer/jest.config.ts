de54b06d0709ac2ccbffa10b7fdfd4958897f262
galczo5 <kamil.galek@gmail.com>
2022-07-23 10:30:01 +0200

    first time comparison esbuild ~5s, ts-jest ~31s

--- apps/jest-ngc-esbuild-transformer/jest.config.ts
+++ apps/jest-ngc-esbuild-transformer/jest.config.ts
@@ -12,3 +12,27 @@ export default {
     'jest-preset-angular/build/serializers/html-comment',
   ],
 };
+
+// OLD FILE - UNCOMMENT IF YOU WANT TO COMPARE TIME
+// /* eslint-disable */
+// export default {
+//   displayName: 'jest-ngc-esbuild-transformer',
+//   preset: '../../jest.preset.js',
+//   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+//   globals: {
+//     'ts-jest': {
+//       tsconfig: '<rootDir>/tsconfig.spec.json',
+//       stringifyContentPathRegex: '\\.(html|svg)$',
+//     },
+//   },
+//   coverageDirectory: '../../coverage/apps/jest-ngc-esbuild-transformer',
+//   transform: {
+//     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+//   },
+//   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+//   snapshotSerializers: [
+//     'jest-preset-angular/build/serializers/no-ng-attributes',
+//     'jest-preset-angular/build/serializers/ng-snapshot',
+//     'jest-preset-angular/build/serializers/html-comment',
+//   ],
+// };
